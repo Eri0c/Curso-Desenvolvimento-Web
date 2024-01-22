71b9fac2313ef1ade1b976363dce2f6badaf7217
Eric Moraes <118567658+Eri0c@users.noreply.github.com>
2024-01-22 20:25:28 -0300

    Sistema de sliders

--- index.php
+++ index.php
@@ -85,6 +85,11 @@
    </footer>
    <script src="<?php echo INCLUDE_PATH; ?>js/jquery.js"></script>
    <script src="<?php echo INCLUDE_PATH; ?>js/scripts.js"></script>
+    <?php 
+        if($url =='home' || $url == ''){
+    ?>
+    <script src="<?php echo INCLUDE_PATH; ?>js/slider.js"></script>
+    <?php } ?>
    <?php 
         if($url == 'contato'){
     ?>
--- pages/home.php
+++ pages/home.php
@@ -1,4 +1,8 @@
-<section class="banner-principal">
+<section class="banner-container">
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form.jpg');" class="banner-single"></div><!--banner-single-->
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/image1.jpg');" class="banner-single"></div><!--banner-single-->
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/image2.jpg');" class="banner-single"></div><!--banner-single-->
+    <div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg.jpg');" class="banner-single"></div><!--banner-single-->
         <div class="overlay"></div><!--overlay-->
         <div class="center">
         <form>
